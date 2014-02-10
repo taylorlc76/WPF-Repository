55d13b418e01dcd03fc470bfc74f1b270d199e43
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-10 16:58:37 -0600

    Follow along script for Screencast: Boolean and String variables

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,5 +1,10 @@
+//Variables of Different Types, Numbers, Booleans and Strings
 
-var age;
-	age = 26;
+var age = 26; // Number Variables
 
-var yearOfBirth = 1987;
+var isStudent = false; //Boolean Variables
+
+var firstName = "Lindsay"; //String Variable 
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor;
+

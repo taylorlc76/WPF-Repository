a54fa185c1100b4a7e6ab21485958dfc7f1c8e01
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-10 17:16:39 -0600

    Follow along script for Screencast: Example Expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,5 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //sets up our variable a and defines it with 2
-var b;
-b = a + 3; //add 3 to a
-console.log(a);
+var yearBorn = 1987;
+var age = 2013 - yearBorn;
+console.log(age);

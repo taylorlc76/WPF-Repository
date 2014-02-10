251561331fc041e66c462219fdfed95d1fe0e9d3
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-10 17:12:41 -0600

    Follow along script for Screencast: Basic Expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,6 @@
-//Arrays
+//Expressions
 
-var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
-
-muppetNames[1] = "Rizzo"; //lets me see whats in the index #1
-muppetNames[2] = "Fozzy";
-
-var num =1;
-
-console.log(muppetNames[1]); //change index #1 to Rizzo
+var a = 2; //sets up our variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(a);

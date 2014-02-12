116adc55d30460bcb6b5b35f3e7fccf75e24ee4c
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-12 17:01:29 -0600

    Following along for screencast: casting

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,8 @@
-//Expressions - Concatenating
+//Expressions - Casting
 
-var a = 6;
-var b = 7;
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
 
-var result = a + b;
-console.log(result);
-
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" +String(secPart);
+console.log(phoneNo);

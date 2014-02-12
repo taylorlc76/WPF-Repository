4b325c4e20137a5271e107795764db0ba0adf7d4
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-12 17:09:11 -0600

    Follow along for screencast: assignment operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,6 @@
-//Expressions - Casting
+//Expressions - Assignment Operators
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" +String(secPart);
-console.log(phoneNo);
+var a = 3;
+//a ++ ; //a = a + 1 // a += 1
+a ++; //a = a - 1 // a -=1
+console.log(a);

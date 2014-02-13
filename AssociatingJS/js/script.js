57466b1f51df95ba8a37b9d95871c386302d53d0
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-13 14:58:01 -0600

    Follow along for screencast: prompts and alerts.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,8 @@
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-var a = 3;
-//a ++ ; //a = a + 1 // a += 1
-a ++; //a = a - 1 // a -=1
-console.log(a);
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
+var area = width + height; //calculates with that info
+//console.log(area); //prints it out to console
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);

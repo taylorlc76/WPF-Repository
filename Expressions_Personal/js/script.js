c6eebd5aa80db6f226a3e8928c83e1dc0c9291f6
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-13 18:39:31 -0600

    Added comment of the purpose of the second prompt towards the user

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,4 +4,5 @@
 
 var girls = prompt("Enter number of girls?"); //this allows for the user to be prompted to enter in the number of girls needing makeup done for the show
 
-var minutes = prompt("How many minutes needed for each?");
+var minutes = prompt("How many minutes needed for each?"); //this allows for the user to input how many minutes they will allot for each girl for makeup
+

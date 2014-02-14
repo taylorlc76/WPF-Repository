1dd979313513273777b583b39a8338b89d33291c
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 08:28:19 -0600

    Added header to script and the problem attempting to solve

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,3 @@
+//Lindsay Taylor, 02/14/2014, Expressions Assignment
+
+//How many lightbulbs does it take to blow a fuse using wattage of lightbulb, the amperes of a fuse, and the volts of the outlets

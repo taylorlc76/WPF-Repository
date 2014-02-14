2bf829bc8df8eb4b2804d27c34b727692a345ad8
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 15:10:25 -0600

    Commented out Dog Years to work on Slice of Pie part 1. Added variables and calculations for new problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,11 +2,22 @@
 
 //Dog Years
 
-var humanYears = 1; //this is the variable that changes based on how many human years old the dog is
+//var humanYears = 1; //this is the variable that changes based on how many human years old the dog is
 
-dogYears = 7; //this is the variable that does not change 
+//dogYears = 7; //this is the variable that does not change 
 
-var result = humanYears * dogYears; //this variable shows that based on the number of human years the dog is, it will always be calculated against 7 years to get the age of the dog in dog years
+//var result = humanYears * dogYears; //this variable shows that based on the number of human years the dog is, it will always be calculated against 7 years to get the age of the dog in dog years
 
-console.log("Sparky is " + humanYears + " human years old which is " + result + " in dog years."); //this prints out to the console the statement "Sparky is X human years old which is X in dog years.".
+//console.log("Sparky is " + humanYears + " human years old which is " + result + " in dog years."); //this prints out to the console the statement "Sparky is X human years old which is X in dog years.".
 
+//Slice of Pie part 1
+
+var partyPeople = 5;
+
+var numOfPizzas = 10;
+
+slicesPerPizza = 8;
+
+var result = (numOfPizzas * slicesPerPizza) / partyPeople;
+
+console.log("Each person ate " + result + " slices of pizza at the party.");

e767aac4b4d81ce27735d2874c506eb7d3b9976e
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 08:05:22 -0600

    Added commentary for variable result

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,4 +12,4 @@ var wholeGrounds = bag / 16; //this allows for me to calculate the amount of oun
 
 var numOfBrews = wholeGrounds / groundCoffee; //this caclulates the number of brews made from one whole bag of coffee beans
 
-var result = numOfBrews * liquidCoffee;
+var result = numOfBrews * liquidCoffee; //this calculates the answer to my promblem of how much coffee is made from one bag of beans

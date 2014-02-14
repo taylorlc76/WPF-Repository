1e367172007d37e6cb4dd09d14041537b8c33356
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 15:04:15 -0600

    Added commentary for each line of script for problem Dog Years

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,11 +2,11 @@
 
 //Dog Years
 
-var humanYears = 1;
+var humanYears = 1; //this is the variable that changes based on how many human years old the dog is
 
-var dogYears = 7;
+dogYears = 7; //this is the variable that does not change 
 
-var result = humanYears * dogYears;
+var result = humanYears * dogYears; //this variable shows that based on the number of human years the dog is, it will always be calculated against 7 years to get the age of the dog in dog years
 
-console.log("Sparky is " + humanYears + " human years old which is " + result + " in dog years.");
+console.log("Sparky is " + humanYears + " human years old which is " + result + " in dog years."); //this prints out to the console the statement "Sparky is X human years old which is X in dog years.".
 

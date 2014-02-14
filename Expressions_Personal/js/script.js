95a5cf1ef229178cff4ebd43bd9a9c88ef59d78d
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-13 18:41:27 -0600

    Added variable as the assignment operator for the third prompt regarding transitions

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,4 +6,4 @@ var girls = prompt("Enter number of girls?"); //this allows for the user to be p
 
 var minutes = prompt("How many minutes needed for each?"); //this allows for the user to input how many minutes they will allot for each girl for makeup
 
-prompt("How many transitions will there be during the show?");
+var transitions = prompt("How many transitions will there be during the show?");

e1ef3d5d27451d5850df0b874c1fd6dca690ed52
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 08:29:39 -0600

    Added prompt for user to enter in other 15 or 25 for the amount of amperes of their fuse

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,5 @@
 //Lindsay Taylor, 02/14/2014, Expressions Assignment
 
 //How many lightbulbs does it take to blow a fuse using wattage of lightbulb, the amperes of a fuse, and the volts of the outlets
+
+prompt("Please input below if your fuse uses either 15 or 15 amperes");

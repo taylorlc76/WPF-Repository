8c8beda03a2dda96cecab1c3430e5f0cf99052bd
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 08:39:07 -0600

    Added commentary to variable prompt for outletVolts

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,6 +4,7 @@
 
 var amperes = prompt("Please input below if your fuse uses either 15 or 15 amperes"); //this will allow for me to calculate the amperes of the fuse box with the lightbuls in the future
 
-var lightbulbWattage = prompt("What is the wattage of the lightbulbs?"); //thsi allows for me to use the user's input of the wattage of each lightbulb for future calculation
+var lightbulbWattage = prompt("What is the wattage of the lightbulbs?"); //this allows for me to use the user's input of the wattage of each lightbulb for future calculation
 
-var outletVolts = prompt("Please input below if your outlets use 120 or 240 volts");
+var outletVolts = prompt("Please input below if your outlets use 120 or 240 volts"); //this allows for me to use either 120 ro 240 volts inputted from user for future calculation
+

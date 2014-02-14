9176ad317c48d0843ada996808feb96bfb66967b
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 07:42:52 -0600

    Added commentary explaining the reasoning and usage of the first variable and prompt.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,4 +2,5 @@
 
 //How much can a bag of whole coffee beans make into fresh, hot coffee using size of bag of beans, amount each brew makes into liquid, amount of grounded beans needed for each brew.
 
-var bag = prompt("How many pounds of whole beans is in one bag?");
+var bag = prompt("How many pounds of whole beans is in one bag?"); //this allows for the user to be prompted to enter in how many pounds of beans are in one bag for coffee and the variable allows me to use that information into my calculation
+

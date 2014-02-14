e91cc256a783a1ea71dc75671fdafd817406332f
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-14 15:00:34 -0600

    Added variables for problem Dog Years

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Lindsay Taylor, 02/14/2014, Expressions Worksheet
+
+//Dog Years
+
+var humanYears = 1;
+
+var dogYears = 7;
+
+var result = humanYears * dogYears;
+
+console.log("Sparky is " + humanYears + " human years old which is " + result + " in dog years.");
+

9cd2277f05c16800d46e715601ddbdc1ce5eb872
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-13 18:26:29 -0600

    Added personal calculation used for this assignment.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,4 @@
+//Lindsay Taylor, 02/13/2014, Assignment: Expressions
+
+//Using number of girls, time for each girl, and amount of transitions between performances, I can calculate amount of time spent volunteering at the show doing makeup.
+

27d38919258776a7d3f4c650d5d53643e6c63ca1
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-17 11:17:46 -0600

    Follow along script for screencast: If and Else

--- Week 3 Screencasts/js/script.js
+++ Week 3 Screencasts/js/script.js
@@ -1,11 +1,14 @@
-//Week 2 Screencasts: Condition with Expressions
+//Week 2 Screencasts: If and Else
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2
+
 //if the child is old enough, print to the console "you can ride!"
 //if the kid os over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+}else{
+	//code performed if condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
 }

447553d00eb05a8cfe4a7533d6f65237ce86d010
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-17 11:02:43 -0600

    Added folder "Week 2 Screencasts" for videos and follow alongs. Added follow along for screencast: Conditionals Example

---
+++ Week 3 Screencasts/js/script.js
@@ -0,0 +1,10 @@
+//Week 2 Screencasts: Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+console.log("what comes after");

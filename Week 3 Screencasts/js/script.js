0b7cd57714d2c940056834dad8a66d0b84f6b868
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-17 11:09:10 -0600

    Added follow along script for screencast: Relational Expressions

--- Week 3 Screencasts/js/script.js
+++ Week 3 Screencasts/js/script.js
@@ -1,10 +1,10 @@
-//Week 2 Screencasts: Conditional Logic
+//Week 2 Screencasts: Relational Expressions
 
-var oldEnough = false;
-
+var kidHeight = 50;
+var minHeight = 48;
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid os over 48 inches in height
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
-console.log("what comes after");

cd46efa3b192f1e8b621d898d1b49bcc3997bffa
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 17:14:37 -0600

    Added solutions to problems for assignment Conditionals Worksheet

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,54 @@
+//Lindsay Taylor 02/19/2014 Conditionals Worksheet
+
+//Celsius to Fahrenheit Converter
+
+//var temp = prompt("Please enter in the value of the temperature in need of conversion.");
+
+//var degree = prompt("Please enter in either \"C\" to convert into Celsius or \"F\" for Fahrenheit.");
+
+//var celsisusConvert = (temp * 9) / 5 + 32;
+
+//var fahrenheitConvert = (temp - 32) * 5 / 9;
+
+//if(degree == "C"){
+	//console.log("The temperature is " + fahrenheitConvert + " degrees Celsius.");
+//}else{
+	//console.log("The temperature is " + celsisusConvert + " degrees Fahrenheit.");
+//}
+
+//Check the Login
+
+//var correctUsername = "cherriblossom";
+
+//var correctPassword = "oogabooga";
+
+//var enteredUsername = prompt("Please enter in your username.");
+
+//var enteredPassword = prompt("Please enter in your password.");
+
+//if(correctUsername == enteredUsername && correctPassword == enteredPassword){
+	//console.log("Welcome, " + correctUsername + "!");
+//}
+//if(enteredUsername != correctUsername){
+	//console.log("User not found. Try again.");
+//}
+//if(correctUsername == enteredUsername && correctPassword != enteredPassword){
+	//console.log("Password does not match our records.");
+//}
+
+//Tire Pressure I
+
+var tirePressure = [113, 113, 124, 123];
+
+if(tirePressure[0] == tirePressure[1]){
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
+
+if(tirePressure[2] == tirePressure[3]){
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
+

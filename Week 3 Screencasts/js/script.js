285d0f3df4a2f59d53b15e296fbb85f10d41d070
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 16:46:09 -0600

    Part 1 of 2 followup scripts for screencasts: Logical Operators

--- Week 3 Screencasts/js/script.js
+++ Week 3 Screencasts/js/script.js
@@ -1,18 +1,12 @@
-//Week 2 Screencasts: Else If
+//Week 2 Screencasts: Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid os over 48 inches in height
-if(kidHeight > minHeight){
-	//you can ride!
-	console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-	//you can ride with a parent present
-	console.log("You can ride, but only with a parent present.");
+//if the price of the phone is less our budget AND  if your paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+	console.log("We can buy the phone!") ;
 }else{
-	//sorry you have growing to do
-	console.log("Sorry kid, you've got some growing to do first!");
+	console.log("No phone for you!!");
 }

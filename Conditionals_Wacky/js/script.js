574ab840eefb61db92e691109ecce345ac99451e
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 21:37:51 -0600

    Added initial variables for wacky sub project

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,8 @@
+//Lindsay Taylor 02/20/2014 Conditionals
+
+//Party!
+
+var people = prompt("How many people will come to your party?");
+
+var wine = prompt("How many bottles of wine will you serve?");
+

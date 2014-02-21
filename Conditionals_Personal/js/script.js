5261de0c8d9248855427a2512ffcf4842e580186
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 21:43:21 -0600

    Added commentary for variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,13 +2,13 @@
 
 //Buying a Car
 
-var carPrice = prompt("\"The price of the car I want is...\"");
+var carPrice = prompt("\"The price of the car I want is...\""); //this is the price of the car
 
-var percentDown = prompt("\"I need to put down .__ on the car (convert to decimal).\"");
+var percentDown = prompt("\"I need to put down .__ on the car (convert to decimal).\""); //this is the percent I wish to put down for the car
 
-var months = prompt("\"I need to save up for my percent down in ___ months.\"");
+var months = prompt("\"I need to save up for my percent down in ___ months.\""); //this is how many months I have to save up for the down payment
 
-var actualSaved = prompt("\"I can only save away $___ each month.\"");
+var actualSaved = prompt("\"I can only save away $___ each month.\""); //this is the amount I can save up each month
 
 var downPayment = carPrice * percentDown
 

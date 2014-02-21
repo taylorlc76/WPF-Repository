c3d3d449823bbdadc49443cee361c382d39c19a1
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 21:41:36 -0600

    Added commentary for reach line of script

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,12 +2,12 @@
 
 //Party!
 
-var people = prompt("How many people will come to your party?");
+var people = prompt("How many people will come to your party?"); //this will be used for inputting wine per person
 
-var wine = prompt("How many bottles of wine will you serve?");
+var wine = prompt("How many bottles of wine will you serve?"); //this will be used to calculate ounces for each bottle and for wine per person
 
-var ouncesPerPerson = (wine * 33.8) / people;
+var ouncesPerPerson = (wine * 33.8) / people; // this calculates the ounces of wine each person will received based on user input
 
-var winePerPerson = ouncesPerPerson / 8;
+var winePerPerson = ouncesPerPerson / 8; //this will calculate how many glasses of wine each person will get
 
-(winePerPerson < 2) ? console.log("You need to buy more wine!") : console.log("You have more than enough for each person to have atleast two glasses of wine!");
+(winePerPerson <= 2) ? console.log("You need to buy more wine!") : console.log("You have more than enough for each person to have atleast two glasses of wine!"); //this will print out to the console either one of these statements based on if each persone will get more than or equal to two glasses of wine

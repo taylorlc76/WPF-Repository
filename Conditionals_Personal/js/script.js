0f194f233abdc3da788b0a6424c095fb9842ef71
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 19:44:02 -0600

    Formatted second prompt to have user input percent as a decimal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,9 +4,9 @@
 
 var carPrice = prompt("\"The price of the car I want is...\"");
 
-var percentDown = prompt("\"I need to put down __% on the car\"");
+var percentDown = prompt("\"I need to put down __ on the car (in decimal).\"");
 
-var months = prompt("\"I need to save up for my percent down in ___ months\"");
+var months = prompt("\"I need to save up for my percent down in ___ months.\"");
 
-var actualSaved = prompt("\"I can only save away $___ each month\"");
+var actualSaved = prompt("\"I can only save away $___ each month.\"");
 

5a91f5fd992c74518f9a4d1d1d7489dbeb09d159
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 19:58:16 -0600

    Clarified percentDown variable and added in the amount needing to save up if statement is false

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 var carPrice = prompt("\"The price of the car I want is...\"");
 
-var percentDown = prompt("\"I need to put down __ on the car (in decimal).\"");
+var percentDown = prompt("\"I need to put down .__ on the car (in decimal).\"");
 
 var months = prompt("\"I need to save up for my percent down in ___ months.\"");
 
@@ -16,8 +16,10 @@ var downInMonths = downPayment / months
 
 var actualInMonths = actualSaved * months
 
+var difference = downInMonths - actualInMonths
+
 if(actualInMonths > downInMonths){
 	console.log("\"I will be able to get the car!\"");
 }else{
-	console.log("\"I will need to save up more!\"");
+	console.log("\"I will need to save up " + difference + " more!\"");
 }

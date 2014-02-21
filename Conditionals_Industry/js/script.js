6563dde3b828fca4e6d5160852cb0ab6a2c47fa1
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 20:40:22 -0600

    Added variables for industry related sub project

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,2 +1,23 @@
 //Lindsay Taylor 02/20/2014 Conditionals
 
+//Case of Apples
+
+var caseOfApples = prompt("How many pounds is a case of apples?");
+
+var applesToJuice = prompt("On average, how many apples go into making a large juice?");
+
+var autumnHarvest = prompt("On average, how many apples go into making the Autumn Harvest mix?");
+
+var applesToPie = prompt("On average, how many apples are used to make an apple custard pie?");
+var applePies = prompt("On average, how many apple pies are made each week?");
+
+var applesToOunces = caseOfApples * 16;
+
+var pies = (applePies * applesToPie) * 7;
+
+var harvest = autumnHarvest * 7
+
+var applesAndPies = (applesToOunces - pies)
+
+var total = (applesToOunces - pies) - harvest
+

e97edbe537d9bb17de905fc314f411e603503394
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-20 19:41:31 -0600

    Added initial files for Personal problem and initial variables for buying a car

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,12 @@
+//Lindsay Taylor 02/20/2014 Conditionals
+
+//Buying a Car
+
+var carPrice = prompt("\"The price of the car I want is...\"");
+
+var percentDown = prompt("\"I need to put down __% on the car\"");
+
+var months = prompt("\"I need to save up for my percent down in ___ months\"");
+
+var actualSaved = prompt("\"I can only save away $___ each month\"");
+

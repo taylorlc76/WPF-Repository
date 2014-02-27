049139edb03f27de43c4664e3612ff25cbba5738
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 12:52:11 -0600

    changed function to anonymous function style

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -4,12 +4,12 @@
 
 function circle(radius){
 	var circumference = 2 * radius * 3.14;//calculation used in this function
-	console.log("The circumference of your circle is " + circumference + " inches.");//prints out to console
+	console.log("The circumference of your circle is " + circumference);//prints out to console
 }
 circle(4);//refers to radius to be manipulated in the calculation when invoking the function circle
 
 //Stung!
 
-function death(){
+var stings = calculateStings(bees, pounds){
 	
 }

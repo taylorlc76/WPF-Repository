564f437e34765a537332fa066710eb2e7beb7731
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 16:34:56 -0600

    added prompt for variable bills

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,6 +4,8 @@
 
 var salary = prompt("On average, how much money do you earn each month?");
 
+var bills = prompt("How much do you pay each month on bills? Including car, credit cards, rent, utilities, ect.");
+
 function(){
 
 }

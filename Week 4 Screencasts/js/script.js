9abf6443df99383c8b8adef73ea1877dacbebbd6
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 08:17:35 -0600

    Added initial files for screencast follow alongs and script for Basic Function Structure

---
+++ Week 4 Screencasts/js/script.js
@@ -0,0 +1,12 @@
+//Week 4 Screencasts: Basic Function Structure
+
+function outptMsg(){
+	console.log("Hello World");
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}

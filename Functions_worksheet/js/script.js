b93665675c67b826e55a659e54866db7d7480754
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 12:42:59 -0600

    added commentary for first problem in worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,7 +1,7 @@
 //Lindsay Taylor 02/27/2014 Functions Worksheet
 
 function circle(radius){
-	var circumference = 2 * radius * 3.14;
-	console.log("The circumference of your circle is " + circumference + " inches.");
+	var circumference = 2 * radius * 3.14;//calculation used in this function
+	console.log("The circumference of your circle is " + circumference + " inches.");//prints out to console
 }
-circle(4);
+circle(4);//refers to radius to be manipulated in the calculation when invoking the function circle

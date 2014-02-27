fe9c9692940a0358bd8e316ef128ea7f9cab74d6
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 12:41:23 -0600

    removed all script and added new function with one line of code and one variable the rest are givens

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,11 +1,7 @@
 //Lindsay Taylor 02/27/2014 Functions Worksheet
-var radius = 4;
 
-function circle(d, p){
-	var d = 2 * radius;
-	var circumference = d * p
-	var p = 3.14;
-	
+function circle(radius){
+	var circumference = 2 * radius * 3.14;
+	console.log("The circumference of your circle is " + circumference + " inches.");
 }
-circle(d, p);
-console.log(circle);
+circle(4);

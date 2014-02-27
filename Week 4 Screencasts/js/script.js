dcb787fb779a41f013f77ed797bf02c07b2b3277
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 08:24:45 -0600

    Added Follow along script for screencast Variable Scope

--- Week 4 Screencasts/js/script.js
+++ Week 4 Screencasts/js/script.js
@@ -1,16 +1,12 @@
-//Week 4 Screencasts: Function Invocation
+//Week 4 Screencasts: Variable Scope
 
-function outptMsg(){
-	console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
 	var width = 20;
 	var height = 30;
 	var area = width * height;
-	console.log(area);
+	//console.log(area);
 }
-
 calcArea();
-calcArea();
-calcArea();
+console.log(width);

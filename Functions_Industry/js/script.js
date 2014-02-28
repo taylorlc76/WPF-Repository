2238d909f84c50fdbf7cb5f612ccb4ac82d44eeb
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 21:38:02 -0600

    changed if and else if variables to same variable for further calculation. changed variable to find the total, inputted price of each item

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,24 +5,20 @@ var scones = prompt("How many organic scones would you like?");
 var cinnamonRolls = prompt("How many organic cinnamon rolls would you like?");
 
 var croissants = prompt("Would you like a \"flavored\", or a \" plain\" croissant?");
-if(croissants = flavored){
-	var flavoredNum = prompt("How many would you like?");
-}else if(croissants = plain){
-	var plainNum = prompt("How many would you like?");
+
+if(croissants == "flavored"){
+	var croissantNum = prompt("How many would you like?");
+}else if(croissants == "plain"){
+	var croissantNum = prompt("How many would you like?");
 }else{
 	alert("You did not input either flavored or plain!");
 }
-sconePrice = scones * 2;
 
-cinnRollPrice = cinnamonRolls * 1.5;
 
-flavoredPrice = flavoredNum * 3;
-
-plainPrice = plainNum * 2.5;
-
-function pastries(tax){
-	var goodies = (sconePrice + cinnRollPrice + flavoredPrice + plainPrice) * tax;
-	console.log("The subtotal for your pastries is $" + goodies + ". The total with tax is $" + total +".");
+function pastries(sconePrice, cinnPrice, croissantPrice){
+	var goods = (scones * sconePrice) + (cinnamonRolls * cinnPrice) + (croissantNum * croissantPrice);
+	console.log("The total for your pastries is $" + goods + ".");
+	
 }
 
-var total = pastries(1.07);
+var total = pastries(2, 1.50, 3);

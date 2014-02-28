0d99b5758cdd5098b3fab9abbf478cb35d5799a0
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 18:59:41 -0600

    added new line to prompts, added console.log script

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,14 +4,14 @@
 
 var salary = prompt("On average, how much money do you earn each month?");
 
-var bills = prompt("How much do you pay each month on bills? \n Including car, credit cards, rent, utilities, ect.");
+var bills = prompt("How much do you pay each month on bills? \nIncluding car, credit cards, rent, utilities, ect.");
 
-var necessities = prompt("On average, how much do you spend each month for necessities? \n Such as toiletries, self care, prescriptions, ect.");
+var necessities = prompt("On average, how much do you spend each month for necessities? \nSuch as toiletries, self care, prescriptions, ect.");
 
-var food = prompt("How much do you spend on food each month? \n Including dining out and buying at the grocery store.");
+var food = prompt("How much do you spend on food each month? \nIncluding dining out and buying at the grocery store.");
 
 function budget(){
 	var month = salary - bills - necessities - food;
-	var year = month * 12
-	console.log("You have $" + month + " left for spending or saving each month or you have $" + year + " left each year.");
+	return month;
 }
+console.log("You have $" + month + " left for spending or saving each month or you have $" + year + " left each year.")

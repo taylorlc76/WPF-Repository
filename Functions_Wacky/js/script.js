5183d7eeca3d5a1336ae6055ccd895fb0298f13b
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 22:18:22 -0600

    Added "!" to complete the sentence for line 3

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,6 +1,6 @@
 //Lindsay Taylor 02/27/2014 Functions Assignment
 
-alert("Welcome to the Diapers of Doom, to enter you must be of legal age");//this alerts the user when they enter the page
+alert("Welcome to the Diapers of Doom, to enter you must be of legal age!");//this alerts the user when they enter the page
 
 var age = prompt("How old are ye?");//this prompts the user to enter in their age
 

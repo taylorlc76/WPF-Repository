c819505f5fdcd5d6a53a59005d96526fa72541fc
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 21:03:51 -0600

    added variable prompts for user. added if and else if statement for type of croissants user wants

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,1 +1,14 @@
 //Lindsay Taylor 02/27/2014 Functions Assignment
+
+var scones = prompt("How many organic scones would you like?");
+
+var cinnamonRolls = prompt("How many organic cinnamon rolls would you like?");
+
+var croissants = prompt("Would you like a \"flavored\", or a \" plain\" croissant?");
+if(croissants = flavored){
+	var flavoredNum = prompt("How many would you like?");
+}else if(croissants = plain){
+	var plainNum = prompt("How many would you like?");
+}else{
+	alert("You did not input either flavored or plain!");
+}

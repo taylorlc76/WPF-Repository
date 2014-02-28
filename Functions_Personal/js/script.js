be295ee2eb4d624d8bd18cd45a120e2d589fc6da
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 20:29:45 -0600

    added food variable to necessities to cut down on variables. removed food variable. made variable into anonymous variable and had it calculate only yearly instead of monthly budget.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,12 +6,13 @@ var salary = prompt("On average, how much money do you earn each month?");
 
 var bills = prompt("How much do you pay each month on bills? \nIncluding car, credit cards, rent, utilities, ect.");
 
-var necessities = prompt("On average, how much do you spend each month for necessities? \nSuch as toiletries, self care, prescriptions, ect.");
+var necessities = prompt("On average, how much do you spend each month for necessities? \nSuch as toiletries, self care, prescriptions, food, ect.");
 
-var food = prompt("How much do you spend on food each month? \nIncluding dining out and buying at the grocery store.");
-
-function budget(){
-	var month = salary - bills - necessities - food;
-	return month;
+var budget = function(year){
+	var yearly = (salary - bills - necessities) * year;
+	return yearly;
 }
-console.log("You have $" + month + " left for spending or saving each month or you have $" + year + " left each year.")
+
+var y = budget(12);
+
+console.log("You have $" + y + " left for spending or saving each year");

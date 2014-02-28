160f91fb9831fb86af49749e61e15e4ac55fc1b5
Lindsay Taylor <shinxiah@fullsail.edu>
2014-02-27 22:16:23 -0600

    added commentary for line scripts

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,11 +1,12 @@
 //Lindsay Taylor 02/27/2014 Functions Assignment
 
-alert("Welcome to the Diapers of Doom, to enter you must be of legal age");
+alert("Welcome to the Diapers of Doom, to enter you must be of legal age");//this alerts the user when they enter the page
 
-var age = prompt("How old are ye?");
+var age = prompt("How old are ye?");//this prompts the user to enter in their age
 
 function permission(){
-	(age < 18) ? alert("You are too young to enter!") : alert("You may pass!");
+	(age < 18) ? alert("You are too young to enter!") : alert("You may pass!");//this will give an alert to user based on their age provided
+	return age
 }
 
-permission();
+permission();//this invokes the function
